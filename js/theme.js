1b52d86ca172d703c04fe181c1dc950347636ed1
nt2-ludovic <57907999+nt2-ludovic@users.noreply.github.com>
2019-12-05 10:10:37 -0500

    jquery

--- js/theme.js
+++ js/theme.js
@@ -98,67 +98,7 @@ $(document).ready(function() { "use strict";
     }
   });
 
-/**************************************
- * fullPage jQuery
-***************************************/
 
-    $(document).ready(function() { "use strict";
-        $('#fullpage').fullpage({
-            //Navigation
-            menu: '#menu',
-            lockAnchors: false,
-            anchors:['slide1', 'slide2', 'slide3'],
-            navigation: true,
-            navigationPosition: 'right',
-            navigationTooltips: [''],
-            showActiveTooltip: true,
-            slidesNavigation: true,
-            slidesNavPosition: 'top',
-            //Scrolling
-            css3: true,
-            scrollingSpeed: 700,
-            autoScrolling: true,
-            fitToSection: true,
-            fitToSectionDelay: 1000,
-            scrollBar: false,
-            easing: 'easeInOutCubic',
-            easingcss3: 'ease',
-            loopBottom: false,
-            loopTop: false,
-            loopHorizontal: true,
-            continuousVertical: false,
-            normalScrollElements: '#element1, #element2, #element3, #element4',
-            scrollOverflow: false,
-            touchSensitivity: 15,
-            normalScrollElementTouchThreshold: 5,
-            //Accessibility
-            keyboardScrolling: true,
-            animateAnchor: true,
-            recordHistory: true,
-            //Design
-            controlArrows: true,
-            verticalCentered: true,
-            resize : false,
-            sectionsColor : ['#ccc', '#eee'],
-            paddingTop: '3em',
-            paddingBottom: '10px',
-            fixedElements: '#header, .footer',
-            responsiveWidth: 1100,
-            responsiveHeight: 0,
-            //Custom selectors
-            sectionSelector: '.section',
-            slideSelector: '.slide',
-            //events
-            onLeave: function(index, nextIndex, direction){},
-            afterLoad: function(anchorLink, index){},
-            afterRender: function(){},
-            afterResize: function(){},
-            afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-            onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
-        });
-		return false;
-    });
-	
 /**************************************
  * isotope jQuery
 ***************************************/
