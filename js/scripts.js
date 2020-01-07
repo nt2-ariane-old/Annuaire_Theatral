9c8a7525d7b0b21b8efccab7e6ef9c3f4a12f1f8
Ludovic Doutre-Guay <ludovic@labo-nt2.org>
2020-01-07 11:15:40 -0500

    test

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+window.onload() {
+        let rows = document.querySelectorAll('.views-row');
+        rows.forEach(row => {
+                if (!empty(row.querySelectorAll('.blog-zig-zag'))) {
+                    let odd = row.querySelector('space-mobile:odd');
+                    odd.forEach(element => {
+                        odd.style.backgroundColor = "red";
+                    });
+                }
+            }
+        }
