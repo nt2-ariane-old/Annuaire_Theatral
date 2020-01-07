9172f0d7f85e194aea0b05b1a973a43db6d251a9
Ludovic Doutre-Guay <ludovic@labo-nt2.org>
2020-01-07 16:46:52 -0500

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 window.onload = function() {
     let rows = document.querySelectorAll('.views-row');
     rows.forEach(row => {
-        i = 0;
+        i = 1;
         row.style.backgroundColor = 'red';
         rowContent = row.querySelectorAll('.blog-zig-zag');
         if (rowContent.length > 0) {
             if (i % 2 == 0) {
-                row.style.backgroundColor = 'red';
+                row.style.border = '5px solid red';
             } else {
-                row.style.backgroundColor = 'green';
+                row.style.border = '5px solid blue';
 
             }
             i++;
