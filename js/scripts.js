718b4ba65695ecb7b35efe20091814cae51795a3
Ludovic Doutre-Guay <ludovic@labo-nt2.org>
2020-01-07 16:45:12 -0500

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,18 @@
 window.onload = function() {
     let rows = document.querySelectorAll('.views-row');
     rows.forEach(row => {
+        i = 0;
         row.style.backgroundColor = 'red';
-        if (row.querySelectorAll('.blog-zig-zag').length > 0) {
-            let odd = row.querySelector('space-mobile:odd');
-            console.log(odd);
-            let even = row.querySelector('space-mobile:even');
-            console.log(even);
-            odd.forEach(element => {
-                element.style.backgroundColor = "red";
-            });
-            even.forEach(element => {
-                element.style.backgroundColor = "green";
-            });
+        rowContent = row.querySelectorAll('.blog-zig-zag');
+        if (rowContent.length > 0) {
+            if (i % 2 == 0) {
+                row.style.backgroundColor = 'red';
+            } else {
+                row.style.backgroundColor = 'green';
+
+            }
+            i++;
+
         }
     });
 }
